5641412536be71dfb8d361c3f8cb44c2e5e96001
coesensbert <coesensb@incubaid.com>
2023-01-30 15:40:35 +0100

    start and stop scripts for snapshots

---
+++ snapshots/mainnet/startall.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+docker start tfchain-dev-snapshot
+docker start indexer_db_1
+docker start indexer_explorer_1
+docker start indexer_gateway_1
+docker start indexer_ingest_1
+docker start tfchain_graphql_db_1
+docker start tfchain_graphql_query-node_1
+docker start tfchain_graphql_processor_1
