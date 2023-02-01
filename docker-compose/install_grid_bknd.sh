29dbad0f25b4b93c5caac8a5d9c7e6f8f39fe6a0
coesensbert <coesensb@incubaid.com>
2023-02-01 18:49:19 +0100

    caddy + config

--- docker-compose/install_grid_bknd.sh
+++ docker-compose/install_grid_bknd.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 ## Create directories
-mkdir /srv/tfchain /srv/tfchain/chains /srv/tfchain/chains/tfchain_devnet /srv/tfchain/chains/tfchain_devnet/db /srv/indexer /srv/processor ~/grid_snapshots_tmp
+mkdir /srv/tfchain /srv/tfchain/chains /srv/tfchain/chains/tfchain_devnet /srv/tfchain/chains/tfchain_devnet/db /srv/indexer /srv/processor /srv/caddy /srv/caddy/data /srv/caddy/config /srv/caddy/log ~/grid_snapshots_tmp
 
 
 ## Download snapshots, extract and remove archives
