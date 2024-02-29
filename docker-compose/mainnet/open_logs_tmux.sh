49db531bf79811d117f2f8b1a4500505c3d5318e
coesensbert <coesensb@incubaid.com>
2024-03-01 00:18:01 +0100

    improved tmux logs qa/test/main

--- docker-compose/mainnet/open_logs_tmux.sh
+++ docker-compose/mainnet/open_logs_tmux.sh
@@ -1,8 +1,15 @@
 #/bin/bash
-tmux new -d -s tfchain_public_rpc 'docker logs tfchain-public-node -f --tail 500'
-tmux new -d -s graphql_indexer 'docker logs indexer_ingest -f --tail 500'
-tmux new -d -s graphql_processor 'docker logs processor -f --tail 500'
-tmux new -d -s grid_relay 'docker logs grid_relay -f --tail 500'
-tmux new -d -s grid_proxy 'docker logs grid_proxy -f --tail 500'
-tmux new -d -s grid_dashboard 'docker logs grid_dashboard -f --tail 500'
-tmux new -d -s caddy 'docker logs caddy -f --tail 500'
+tmux new -d -s tfchain_public_rpc
+tmux send-keys -t tfchain_public_rpc "docker logs tfchain-public-node -f --tail 500" ENTER
+tmux new -d -s graphql_indexer
+tmux send-keys -t graphql_indexer "docker logs indexer_ingest -f --tail 500" ENTER
+tmux new -d -s graphql_processor
+tmux send-keys -t graphql_processor "docker logs processor -f --tail 500" ENTER
+tmux new -d -s grid_relay
+tmux send-keys -t grid_relay "docker logs grid_relay -f --tail 500" ENTER
+tmux new -d -s grid_proxy
+tmux send-keys -t grid_proxy "docker logs grid_proxy -f --tail 500" ENTER
+tmux new -d -s grid_dashboard
+tmux send-keys -t grid_dashboard "docker logs grid_dashboard -f --tail 500" ENTER
+tmux new -d -s caddy
+tmux send-keys -t caddy "docker logs caddy -f --tail 500" ENTER
