9cd6a0cae453060342371c1a9bc38625fdc51efc
coesensbert <coesensb@incubaid.com>
2024-07-11 23:30:39 +0200

    hub install script changes

--- grid-hub/install-hub.sh
+++ grid-hub/install-hub.sh
@@ -3,13 +3,9 @@
 mkdir -p /srv/0-db_data /srv/0-db_index /srv/0-hub_public/users /srv/0-hub_workdir /srv/caddy/data /srv/caddy/config /srv/caddy/log 
 apt update && apt install python3 python3-redis python3-requests -y
 
-## Disable COW on BTRFS
-chattr +C /srv/0-db_data
-chattr +C /srv/0-db_index
-
-## Get and edit the 0-hub config
-#wget https://raw.githubusercontent.com/threefoldtech/0-hub/master/src/config.py.sample -O /srv/0-hub/config.py
-# nano -w +19 /srv/0-hub/config.py
+## Disable COW on BTRFS (optional in case of btrfs at /srv)
+#chattr +C /srv/0-db_data
+#chattr +C /srv/0-db_index
 
 ## Start Grid backed services with docker-compose
 docker compose --env-file .env up -d
