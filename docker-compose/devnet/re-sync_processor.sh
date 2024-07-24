27d038f0153fdadd194ac3d88a2115bb7c0874e9
Mik-TF <logismos@protonmail.ch>
2024-07-23 23:50:03 -0400

    added mountpoint to docker compose

--- docker-compose/devnet/re-sync_processor.sh
+++ docker-compose/devnet/re-sync_processor.sh
@@ -3,16 +3,16 @@ WD=$(pwd)
 
 # Upgrade and fully re-sync processor from block 0
 # NOTE: make sure the snapshot server is upgraded and re-synced first!
-mkdir ~/grid_processor_tmp
-cd ~/grid_processor_tmp
+mkdir -p /mnt/disk/grid_processor_tmp
+cd /mnt/disk/grid_processor_tmp
 rsync -Lv --progress --partial rsync://bknd.snapshot.grid.tf:34873/gridsnapshotsdev/processor-devnet-latest.tar.gz .
 tar xvf processor-devnet-latest.tar.gz
 rm processor-devnet-latest.tar.gz
 docker stop processor
 docker stop processor_query_node
 docker stop processor_db
-rm -r /srv/processor/*
-mv * /srv/processor/
+rm -r /mnt/disk/srv/processor/*
+mv * /mnt/disk/srv/processor/
 cd "$WD"
 git pull -r
 docker compose --env-file .secrets.env --env-file .env up --no-deps -d processor_db processor processor_query_node
@@ -24,4 +24,4 @@ sleep 30
 docker restart grid_proxy
 
 # Clean up
-rm -r ~/grid_processor_tmp
+rm -r /mnt/disk/grid_processor_tmp
