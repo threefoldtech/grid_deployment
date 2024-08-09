752b8badd29634b4d1f8b01fa4bd670f25b712c6
coesensbert <coesensb@incubaid.com>
2024-08-09 17:42:47 +0200

    make and insert required hub keys in config.py

--- grid-hub/install-hub.sh
+++ grid-hub/install-hub.sh
@@ -1,11 +1,37 @@
 #/bin/bash
 ## prerequisites
 mkdir -p /srv/0-db_data /srv/0-db_index /srv/0-hub_public/users /srv/0-hub_workdir /srv/caddy/data /srv/caddy/config /srv/caddy/log 
-apt update && apt install python3 python3-redis python3-requests -y
+apt update && apt install python3 python3-redis python3-requests python3-pip -y
+pip install pynacl
 
 ## Disable COW on BTRFS (optional in case of btrfs at /srv)
 #chattr +C /srv/0-db_data
 #chattr +C /srv/0-db_index
+
+## Set all required keys in hub config.py
+set -e
+cp config.py-example config.py
+
+### fill in settings
+config_zflist="/usr/bin/zflist"
+
+### generate base64 private key
+pkeyfile=$(mktemp)
+
+openssl genpkey -algorithm x25519 -out ${pkeyfile}
+config_threebot_pkey=$(openssl pkey -in ${pkeyfile} -text | xargs | sed -e 's/.*priv\:\(.*\)pub\:.*/\1/' | xxd -r -p | base64)
+
+rm -f ${pkeyfile}
+
+config_threebot_appid="my.app.id"
+
+config_threebot_seed=$(python3 -c "import nacl; from nacl import utils; print(nacl.utils.random(32))")
+
+# apply config (search and replace)
+sed -i "s#__ZFLIST_BIN__#${config_zflist}#" config.py
+sed -i "s#__THREEBOT_PRIVATEKEY__#${config_threebot_pkey}#" config.py
+sed -i "s#__THREEBOT_APPID__#${config_threebot_appid}#" config.py
+sed -i "s#__THREEBOT_SEED__#${config_threebot_seed}#" config.py ## FIXME
 
 ## Start Grid backed services with docker-compose
 docker compose --env-file .env up -d
