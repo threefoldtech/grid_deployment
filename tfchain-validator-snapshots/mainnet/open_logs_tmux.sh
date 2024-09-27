c876f0081c0b3d888e170b865ca5ed556efefcb4
coesensbert <coesensb@incubaid.com>
2024-09-27 18:37:55 +0200

    correct log container

--- tfchain-validator-snapshots/mainnet/open_logs_tmux.sh
+++ tfchain-validator-snapshots/mainnet/open_logs_tmux.sh
@@ -1,7 +1,3 @@
 #/bin/bash
-tmux new -d -s tfchain_public_rpc
-tmux send-keys -t tfchain_public_rpc "docker logs tfchain-public-node -f --tail 500" ENTER
-tmux new -d -s graphql_indexer
-tmux send-keys -t graphql_indexer "docker logs indexer_ingest -f --tail 500" ENTER
-tmux new -d -s graphql_processor
-tmux send-keys -t graphql_processor "docker logs processor -f --tail 500" ENTER
+tmux new -d -s tfchain_validator
+tmux send-keys -t tfchain_public_rpc "docker logs tfchain-validator -f --tail 500" ENTER
