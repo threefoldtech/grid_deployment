5cfb68629847de2a9aea0606eb7b4d0da99c1a99
coesensbert <coesensb@incubaid.com>
2024-10-11 18:14:14 +0200

    executable and correct tmux session

--- tfchain-validator-snapshots/testnet/open_logs_tmux.sh
+++ tfchain-validator-snapshots/testnet/open_logs_tmux.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 tmux new -d -s tfchain_validator
-tmux send-keys -t tfchain_public_rpc "docker logs tfchain-validator -f --tail 500" ENTER
+tmux send-keys -t tfchain_validator "docker logs tfchain-validator -f --tail 500" ENTER
