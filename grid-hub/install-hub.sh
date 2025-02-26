cbf7d5ebfe7dd9738e145ce8de804ee1eb200795
coesensbert <coesensb@incubaid.com>
2025-02-26 13:46:16 +0100

    add domain var for user sync

--- grid-hub/install-hub.sh
+++ grid-hub/install-hub.sh
@@ -82,7 +82,7 @@ docker compose --env-file .env up -d
 
 ## Populate the users
 tmux new -d -s 0-hub_user_sync
-tmux send-keys -t 0-hub_user_sync "while true; do python3 hub-clone.py https://hub.grid.tf /srv/0-hub_public/users; sleep 10m; done" ENTER
+tmux send-keys -t 0-hub_user_sync "while true; do python3 hub-clone.py https://hub.grid.tf /srv/0-hub_public/users ${DOMAIN}; sleep 10m; done" ENTER
 
 ## Sync with hub.grid.tf and keep in sync
 tmux new -d -s 0-hub_sync
